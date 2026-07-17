b6cab2fa47a324000b7956389440006b1079957c
msmaciej <139684364+msmaciej@users.noreply.github.com>
2026-07-17 15:10:03 +0200

    Add files via upload
    update

--- dist/fitcheck-profiles.gen.php
+++ dist/fitcheck-profiles.gen.php
@@ -9,7 +9,7 @@
  *
  * Any edit you make here is overwritten on the next build. Change the copy
  * in config.json / Tina instead.
- * Generated: 2026-07-17T12:56:36.447Z
+ * Generated: 2026-07-17T13:08:52.459Z
  */
 
 $PROFILES = [
