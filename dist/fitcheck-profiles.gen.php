e8cc0dfaec1ccbe240b1e0536a795c4bef898133
msmaciej <139684364+msmaciej@users.noreply.github.com>
2026-09-18 23:45:53 +0200

    update

--- dist/fitcheck-profiles.gen.php
+++ dist/fitcheck-profiles.gen.php
@@ -9,7 +9,7 @@
  *
  * Any edit you make here is overwritten on the next build. Change the copy
  * in config.json / Tina instead.
- * Generated: 2026-09-18T21:30:12.894Z
+ * Generated: 2026-09-18T21:38:12.294Z
  */
 
 $PROFILES = [
